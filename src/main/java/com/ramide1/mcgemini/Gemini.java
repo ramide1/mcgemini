c9b786180cbc64045e972b90a88ccc76582a6b8c
Ramiro <79822249+ramide1@users.noreply.github.com>
2024-08-08 15:06:26 -0300

    remove { and } from content

--- src/main/java/com/ramide1/mcgemini/Gemini.java
+++ src/main/java/com/ramide1/mcgemini/Gemini.java
@@ -59,7 +59,7 @@ public class Gemini implements CommandExecutor {
                 Pattern pattern = Pattern.compile(regex);
                 Matcher matcher = pattern.matcher(response.toString());
                 if (matcher.find()) {
-                    content = matcher.group(1).replace("\\n", "").replace("\\", "").replace("\"", "");
+                    content = matcher.group(1).replace("\\n", "").replace("\\", "").replace("\"", "").replace("{", "").replace("}", "");
                     newHistory = newHistory + "," + "{\"role\": \"model\",\"parts\": [" + "{\"text\": \"" + content
                             + "\"}" + "]}";
                     saveHistory(sender, newHistory);
